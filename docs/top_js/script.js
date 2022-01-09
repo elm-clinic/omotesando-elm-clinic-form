ed01efb4409b3897f19c3918a7c5fefb59cb24c1
elm-clinic <sho_utsumi@elm-clinic.jp>
2022-01-09 10:41:03 +0900

    おすすめポイントアコーディオン修正

--- docs/top_js/script.js
+++ docs/top_js/script.js
@@ -97,6 +97,33 @@ $(window).on('load', function(){
 });
 
 
+//アコーディオンをクリックした時の動作
+$('.title2').on('click', function() {//タイトル要素をクリックしたら
+	$('.box').slideUp(500);//クラス名.boxがついたすべてのアコーディオンを閉じる
+    
+	var findElm = $(this).next(".box");//タイトル直後のアコーディオンを行うエリアを取得
+    
+	if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+		$(this).removeClass('close');//クラス名を除去    
+	}else{//それ以外は
+		$('.close').removeClass('close'); //クラス名closeを全て除去した後
+		$(this).addClass('close');//クリックしたタイトルにクラス名closeを付与し
+		$(findElm).slideDown(500);//アコーディオンを開く
+	}
+});
+
+//ページが読み込まれた際にopenクラスをつけ、openがついていたら開く動作※不必要なら下記全て削除
+$(window).on('load', function(){
+	$('.accordion-area li:first-of-type section').addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
+	$(".open").each(function(index, element){	//openクラスを取得
+		var Title2 =$(element).children('.title2');	//openクラスの子要素のtitle2クラスを取得
+		$(Title2).addClass('close');				///タイトルにクラス名closeを付与し
+		var Box =$(element).children('.box');	//openクラスの子要素boxクラスを取得
+		$(Box).slideDown(500);					//アコーディオンを開く
+	});
+});
+
+
 //logoの表示
 $(window).on('load',function(){
 	$("#splash").delay(2750).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
